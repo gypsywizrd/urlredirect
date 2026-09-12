16873d11f0e4792cc7d27a0ca9e19da5360b6adb
gypsywizrd <90226538+gypsywizrd@users.noreply.github.com>
2026-09-12 14:52:11 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 export default {
   async fetch(request) {
-    const targetM3U = "https://node-9b5feb01.4587.website/6aa52efb.3be55244i178c3152.e6b1.46add6e2/free.live.espn/960x540.2419k/media.m3u8";
+    const targetM3U = "https://node-5d763f4c.4587.website/6aa61dbc.59af03bci178c3152.e779.59d39725/free.live.espn/960x540.2419k/media.m3u8";
     const headers = new Headers();
     headers.set('location', targetM3U);
     headers.set('access-control-allow-origin', '*');
